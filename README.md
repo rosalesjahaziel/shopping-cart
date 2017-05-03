609e4787153cdcf93b7e264cca58fff4a09f6c32
rosalesjahaziel <rosales.jahaziel.a@gmail.com>
2017-05-02 20:43:12 -0500

    added TODO list, implemented Lists in the shopping cart

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Shopping cart
-A simple script to create a shopping ca
+A simple script to create a shopping cart using list, set and dictionary
+
+# TODO:
+Create branches:octocat: for:
+- [*] shopping cart using lists
+- [ ] shopping cart using sets
+- [ ] shopping cart using dictionary
--- shopping_cart.py
+++ shopping_cart.py
@@ -1,15 +1,32 @@
-def get_item():
-    print "Please enter an item: "
-    return raw_input()
+def get_order():
+    print "[command] [item] (command is a to add, d to delete, q t quit)"
+    line = raw_input()
+
+    command = line[:1]
+    item = line[2:]
+
+    return command, item
+
+def process_order(order, cart):
+    command, item = order
+
+    if command == "a":
+        cart.append(item)
+    elif command == "d" and item in cart:
+        cart.remove(item)
+    elif command == "q":
+        return False
+    
+    return True
 
 def go_shopping():
     cart = []
     
     while True:
-        item = get_item()
-        if item == "":
-            break
-        cart.append(item)
+        order = get_order()
+        if not process_order(order, cart):
+            break 
+
     print cart
     print "Finished!"
 
